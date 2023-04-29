61b87445b821eff30c0a6591d7d170db7f6f935a
Berik <berik_2001_14@gmail.com>
2023-04-30 03:57:09 +0600

    fix

--- models/models.js
+++ models/models.js
@@ -27,8 +27,27 @@ const Product = sq.define('product', {
 
 const Order = sq.define('order', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    products: Product,
-    user: User,
+    products: {
+        id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+        name: {type: DataTypes.STRING, unique: false, allowNull: false},
+        description: {type: DataTypes.JSON, allowNull: true},
+        shortDescription: {type: DataTypes.JSON, allowNull: true},
+        price: {type: DataTypes.INTEGER, allowNull: false},
+        rating: {type: DataTypes.DOUBLE, defaultValue: 5.0},
+        views: {type: DataTypes.INTEGER, defaultValue: 0},
+        img: {type: DataTypes.STRING, allowNull: false},
+        tag: {type: DataTypes.STRING, allowNull: true},
+    },
+    user: {
+        id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+        first_name: {type: DataTypes.STRING, unique: false,},
+        last_name: {type: DataTypes.STRING, unique: false,},
+        birthday: {type: DataTypes.STRING, unique: false,},
+        sex: {type: DataTypes.STRING, unique: false,},
+        email: {type: DataTypes.STRING, unique: true,},
+        password: {type: DataTypes.STRING},
+        role: {type: DataTypes.STRING, defaultValue: "USER"},
+    },
 })
 
 module.exports = {
